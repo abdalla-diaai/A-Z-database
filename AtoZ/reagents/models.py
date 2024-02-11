e707c0d5054fe3332940c07c8fdacf332da1a3a4
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-02-11 16:27:55 +0000

    added model, created app, added template pages.

---
+++ AtoZ/reagents/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser
+from django import forms
+from django.db import models
+from django.conf import settings
+
+
+class User(AbstractUser):
+    pass
