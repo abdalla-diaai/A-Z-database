a2f5f4138a3fb05532e5ec46dd824c1d079834e8
Abdalla Diaai <56742529+abdalla-diaai@users.noreply.github.com>
2024-02-25 11:18:33 +0000

    added more functionality to calculator.

--- AtoZ/reagents/static/reagents/script.js
+++ AtoZ/reagents/static/reagents/script.js
@@ -6,18 +6,38 @@ var phusion = [
     ['Water', 7]
 ]
 
-$('#add-field').on('click', function () {
-    const rxNum = $('#tentacles').val();
-    console.log(rxNum);
+var toughmix = [
+    ['Forward Primer', 0.75],
+    ['Reverse Primer', 0.75],
+    ['ToughMix PCR Master Mix', 12.5],
+    ['Template DNA', 1],
+    ['Water', 10]
+]
 
-    for (let i = 0; i < phusion.length; i++) {
-        let tableRow = $("<tr/>");
-        tableRow.append($("<td>").text(i + 1));
-        tableRow.append($("<td>").text(phusion[i][0]));
-        tableRow.append($("<td>").text(phusion[i][1] * rxNum));
-        $("#table-body").append(tableRow);
-    };
-    $('#perRxn').text('Volume per reaction: 19 µL.');
-});
+var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+var tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function btnClick(polName, polList) {
+    $(`#${polName}`).on('click', function () {
+        $("#table-body").empty();
+        const rxNum = $('#tentacles').val();    
+        for (let i = 0; i < polList.length; i++) {
+            let tableRow = $("<tr/>");
+            tableRow.append($("<td>").text(i + 1));
+            tableRow.append($("<td>").text(polList[i][0]));
+            tableRow.append($("<td>").text(polList[i][1] * rxNum));
+            $("#table-body").append(tableRow);
+        };
+        if (polName === 'phusion') {
+            $('#perRxn').text('Volume per reaction: 19 µL.');
+        } else if (polName === 'toughmix') {
+            $('#perRxn').text('Volume per reaction: 24 µL.');
+        }
+    });
+};
+
+btnClick('phusion', phusion);
+btnClick('toughmix', toughmix);
+
 
 
