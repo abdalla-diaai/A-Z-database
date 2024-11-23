9e78790e90328affadbd44157567492f66449479
Abdalla Diaai <abdalla@Mac.communityfibre.co.uk>
2024-11-23 22:43:42 +0000

    removed transcription code.

--- AtoZ/AtoZ/urls.py
+++ AtoZ/AtoZ/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("reagents.urls")),
+
 ]
 # Serve media files in development
 if settings.DEBUG:  # This is important so that it only happens in development
